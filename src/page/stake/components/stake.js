810a5c3c352ef2e1de2e49fd9e0ccdb44d5f465a
cantsleep <cant_sleep@naver.com>
2022-12-28 14:36:34 +0900

    riskHedge Page Edit

--- src/page/stake/components/stake.js
+++ src/page/stake/components/stake.js
@@ -194,7 +194,7 @@ const EvmosInput = styled.div`
   font-family: Pretendard;
   font-size: min(18px, 4vw);
   font-weight: 400;
-  &: focus {
+  &:focus {
     outline: none;
   }
 `;
@@ -219,12 +219,7 @@ const options = [
   { value: "atom", label: "Atom", udenom: "atom" },
 ];
 
-const stage = [
-  { status: "selectToken" },
-  { status: "setAmount" },
-  { status: "??" },
-  { status: "??" },
-];
+const stage = [{ status: "setAmount" }, { status: "stableHedging" }];
 
 const colourStyles = {
   control: (baseStyles, state) => ({
@@ -283,11 +278,7 @@ const Stake = ({
   }, [stake]);
 
   useEffect(() => {
-    if (stake) {
-      setStageLevel(1);
-    } else {
-      setStageLevel(0);
-    }
+    setStageLevel(0);
   }, [stake]);
 
   const web3 = new Web3(window.ethereum);
@@ -361,14 +352,13 @@ const Stake = ({
               <SelectTokenText>Select Token to stake </SelectTokenText>
               <br />
               <DropboxContainer>
-               
                 <TokenToStake
                   defaultValue={options[0]}
                   options={options}
                   onChange={(e) => handleSelectedOption(e)}
                   styles={colourStyles}
                 ></TokenToStake>
-               
+
                 <SelectButton>Select</SelectButton>
               </DropboxContainer>
             </InputBox>
