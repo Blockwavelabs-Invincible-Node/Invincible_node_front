37b3f1f38f37e2344ef23e0286ab52202802d233
hoony0321 <eogns0321@gmail.com>
2023-01-05 17:48:21 +0900

    ci/cd : pipeline 구축 및 기본 파일 생성

---
+++ gh_deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME="deploy-fe"
+echo "> FE 배포"
+cd /home/ubuntu/deploy-fe
+sudo rm -rf node_modules/
+sudo npm install
+sudo kill $(sudo lsof -t -i:3000)
+sudo nohup npx serve -s build
+
+
